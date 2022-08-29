18dda0f4a22c52612c477fb6cc15e6c9df358565
eds9nsilva <antonioedson99@gmail.com>
2022-08-29 16:06:58 -0300

    feat: page Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,74 @@
 import React from 'react';
-import {Container} from './styles';
-import Header from '../../components/Header/Header';
+import {Container, ViewListTask} from './styles';
+import {ScrollView} from 'react-native';
+import Status from '../../components/Status/Status';
+import Button from '../../components/Button/Button';
+import Tasks from '../../components/Tasks/Tasks';
 
 const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Header />
+      <Button />
+      <Status />
+      <ScrollView>
+        <ViewListTask>
+          <Tasks
+            color="#ff0000"
+            title="Fazer atividade de fisica e de historia ao mesmo tempo"
+            date="12/11 as 18:00h"
+            priority="Alta"
+            description="Fazer atividade de fisica e de historia aoFazer atividade de fisica e de historia ao  Fazer atividade de fisica e de historia ao "
+          />
+          <Tasks
+            color="#366EFF"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Baixa"
+          />
+          <Tasks
+            color="#366EFF"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Baixa"
+          />
+          <Tasks
+            color="#ffff00"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Média"
+          />
+          <Tasks
+            color="#ff0000"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Alta"
+          />
+          <Tasks
+            color="#366EFF"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Baixa"
+          />
+          <Tasks
+            color="#ff0000"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Alta"
+          />
+          <Tasks
+            color="#ff0000"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Alta"
+          />
+          <Tasks
+            color="#ffff00"
+            title="Fazer atividade"
+            date="12/11 as 18:00h"
+            priority="Média"
+          />
+        </ViewListTask>
+      </ScrollView>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components/native';
+import {colors} from '../../styles/colors';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
+`;
+
+export const Content = styled.View`
+  position: absolute;
+  bottom: 0%;
+  background-color: ${colors.secundary};
+  width: 100%;
+  height: 80%;
+  border-radius: 22px;
+`;
+
+export const ViewListTask = styled.View`
+  width: 100%;
+  align-items: center;
 `;
